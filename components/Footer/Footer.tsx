ff58ce669b4adb2420fe9633c7b6f8d562d1f274
Andreitsiv <andreitsivwork@gmail.com>
2020-12-31 15:41:48 +0200

    changes

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,10 +1,16 @@
 import { AppBar } from "@material-ui/core";
 import React from "react";
 
-const Footer: React.FC = () => (
-  <footer>
-    <AppBar style={{ height: "80px" }} position="static" />
-  </footer>
-);
+import { useStyles } from "./Footer.styled";
+
+const Footer: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <footer className={classes.footer}>
+      <AppBar style={{ height: "80px" }} position="static" />
+    </footer>
+  );
+};
 
 export default Footer;
